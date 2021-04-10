67861235ca81d7ca3483c1ca3d7c2a93d5ed15ef
METE <METE@METU>
2021-04-11 01:05:07 +0300

    Add project files.

---
+++ group-web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace labtest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult login()
+        {
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "About AppFactory student  team.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "contact us by the  following address.";
+
+            return View();
+        }
+    }
+}
